b39a49326fa9bcc44d25d997452031d72b2f48df
Jamie Macey <jamie@tracefunc.com>
2018-09-18 15:16:40 -0700

    Replace removed undent with proper heredoc

--- HomebrewFormula/elasticsearch@1.7.rb
+++ HomebrewFormula/elasticsearch@1.7.rb
@@ -74,7 +74,7 @@ class ElasticsearchAT17 < Formula
 
   plist_options :manual => "elasticsearch --config=#{HOMEBREW_PREFIX}/opt/elasticsearch@1.7/config/elasticsearch.yml"
 
-  def plist; <<-EOS.undent
+  def plist; <<~EOS
       <?xml version="1.0" encoding="UTF-8"?>
       <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
       <plist version="1.0">
